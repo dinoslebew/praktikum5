d1846216ce18fa995cf3d99c3eecba4ac626be36
dinoslebew <awdino966@gmail.com>
2025-05-21 15:52:01 +0700

    update praktikum 8

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\product;
+use App\Http\Controllers\ListProdukController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +17,5 @@ Route::get('/list_product', [product::class, 'list']);
 Route::get('/home', function () {
     return view('pages.home');
 });
+
+Route::get('/listproduk', [ListProdukController::class, 'show']);
